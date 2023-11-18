232a038b11f3a1db1b2dc6366e785dc1fa4c1028
kwujciak <60245413+kwujciak@users.noreply.github.com>
2023-11-18 11:15:59 -0500

    Update poweroff_service.py

--- poweroff_service.py
+++ poweroff_service.py
@@ -2,16 +2,17 @@ import rclpy
 from rclpy.node import Node
 from irobot_create_msgs.srv import RobotPower
 
+namespace = '/Moondancer'
+
 class RobotPowerService(Node):
     def __init__(self):
         super().__init__('robot_power_service')
-        self.service = self.create_service(RobotPower, '/robot_power', self.turn_off_robot)
+        self.service = self.create_service(RobotPower, namespace + '/robot_power', self.turn_off_robot)
 
-    def turn_off_robot(self, request, response):
-        # Assuming that turning off the robot involves some specific actions
-        # You can add your implementation here
-
-        # For now, let's print a message
+    def turn_off_robot(self, response):
+        #def turn_off_robot(self, request, response):
+        print('here')
+        
         self.get_logger().info('Turning off the robot')
         
         # Populate the response (optional)
@@ -20,10 +21,15 @@ class RobotPowerService(Node):
 
         return response
 
+
 def main(args=None):
     rclpy.init(args=args)
     robot_power_service = RobotPowerService()
-    rclpy.spin(robot_power_service)
+    future = robot_power_service.turn_off_robot(response)
+    print(future)
+    rclpy.spin_until_future_complete(robot_power_service, future)
+    #rclpy.shutdown()
+    #rclpy.spin(robot_power_service)
     rclpy.shutdown()
 
 if __name__ == '__main__':
