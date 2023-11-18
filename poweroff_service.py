20f65f5770f6f5f37dccaa1a56438ed7c6a240de
kwujciak <60245413+kwujciak@users.noreply.github.com>
2023-11-18 11:17:55 -0500

    Update poweroff_service.py

--- poweroff_service.py
+++ poweroff_service.py
@@ -8,28 +8,30 @@ class RobotPowerService(Node):
     def __init__(self):
         super().__init__('robot_power_service')
         self.service = self.create_service(RobotPower, namespace + '/robot_power', self.turn_off_robot)
+        self.future = None  # Initialize the future here
 
-    def turn_off_robot(self, response):
-        #def turn_off_robot(self, request, response):
+    def turn_off_robot(self, request, response):
         print('here')
-        
         self.get_logger().info('Turning off the robot')
         
         # Populate the response (optional)
         response.success = True
         response.message = 'Robot turned off successfully'
 
-        return response
-
+        # Signal that the service has completed
+        self.future.set_result(response)
 
 def main(args=None):
     rclpy.init(args=args)
     robot_power_service = RobotPowerService()
-    future = robot_power_service.turn_off_robot(response)
-    print(future)
-    rclpy.spin_until_future_complete(robot_power_service, future)
-    #rclpy.shutdown()
-    #rclpy.spin(robot_power_service)
+
+    # Create a future and store it in the service
+    robot_power_service.future = robot_power_service.create_service_future(RobotPower.Response)
+
+    # Spin until the service is called
+    rclpy.spin_until_future_complete(robot_power_service, robot_power_service.future)
+
+    # Shutdown
     rclpy.shutdown()
 
 if __name__ == '__main__':
