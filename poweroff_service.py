056906eef3bda81f90848f6af51a6d040d7267a6
kwujciak <60245413+kwujciak@users.noreply.github.com>
2024-05-28 14:35:31 -0400

    Update poweroff_service.py

--- poweroff_service.py
+++ poweroff_service.py
@@ -2,36 +2,32 @@ import rclpy
 from rclpy.node import Node
 from irobot_create_msgs.srv import RobotPower
 
-namespace = '/Moondancer'
+# namespace = '/Moondancer'
 
 class RobotPowerService(Node):
     def __init__(self):
         super().__init__('robot_power_service')
-        self.service = self.create_service(RobotPower, namespace + '/robot_power', self.turn_off_robot)
-        self.future = None  # Initialize the future here
+        self.service = self.create_service(RobotPower, '/robot_power', self.turn_off_robot)
+        self.future = None
 
     def turn_off_robot(self, request, response):
         print('here')
         self.get_logger().info('Turning off the robot')
         
-        # Populate the response (optional)
         response.success = True
         response.message = 'Robot turned off successfully'
 
-        # Signal that the service has completed
         self.future.set_result(response)
+        print(2)
 
 def main(args=None):
     rclpy.init(args=args)
     robot_power_service = RobotPowerService()
+    print(1)
 
-    # Create a future and store it in the service
     robot_power_service.future = robot_power_service.create_service_future(RobotPower.Response)
-
-    # Spin until the service is called
     rclpy.spin_until_future_complete(robot_power_service, robot_power_service.future)
 
-    # Shutdown
     rclpy.shutdown()
 
 if __name__ == '__main__':
