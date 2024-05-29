ce7034dbe9d6f2834ac9f0fbb9610c7e945e5271
kwujciak <60245413+kwujciak@users.noreply.github.com>
2024-05-29 15:01:26 -0400

    Update color.py

--- color.py
+++ color.py
@@ -13,13 +13,13 @@ robot = Root(Bluetooth())
 def get_color_name(r, g, b):
     if r > 200 and g > 200 and b > 200:
         return 'White'
-    elif r > 200 and g < 50 and b < 50:
+    elif r > 200 and g < 50 and b < 100:
         return 'Red'
-    elif r < 50 and g > 200 and b < 50:
+    elif r < 50 and g > 200 and b < 100:
         return 'Green'
     elif r < 50 and g < 50 and b > 200:
         return 'Blue'
-    elif r > 200 and g > 200 and b < 50:
+    elif r > 600 and g > 200 and b < 100:
         return 'Yellow'
     elif r > 200 and g < 50 and b > 200:
         return 'Magenta'
@@ -32,31 +32,31 @@ def get_color_name(r, g, b):
 
 @event(robot.when_play)
 async def play(robot):
-    RGB = []
+    raw_values = []
     while True:
-        while len(RGB) < 5:
+        while len(raw_values) < 5:
             #RGB = []
             # get raw values under all lighting conditions
             for c in Root.ColorLighting:
-                print(c, await robot.get_color_values(c, Root.ColorFormat.ADC_COUNTS))
-                #print(type(await robot.get_color_values(c, Root.ColorFormat.ADC_COUNTS)))
+                #print(c, await robot.get_color_values(c, Root.ColorFormat.ADC_COUNTS))
                 summation = sum(await robot.get_color_values(c, Root.ColorFormat.ADC_COUNTS))
                 #print('avg is ' + str(summation/32))
                 mean = summation/32
-                RGB.append(mean)
+                raw_values.append(mean)
                 #RGB = RGB + [mean]
-                print(RGB)
-                #print(RBG)
+                #print(RGB)
 
             # get robot's parsed colors
-            print("IDs", await robot.get_color_ids())
+            #print("IDs", await robot.get_color_ids())
             await robot.wait(1)
-        red = RGB[1]
-        green = RGB[2]
-        blue = RGB[3]
+        red = raw_values[1]
+        green = raw_values[2]
+        blue = raw_values[3]
+        RGB = [red, green, blue]
+        print(RGB)
 
         color_name = get_color_name(red, green, blue)
         print(f'Color: {color_name}')
-        RGB = []
+        raw_values = []
 
 robot.play()
